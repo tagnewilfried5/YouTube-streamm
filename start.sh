d60706b87b0868de24385b0746b53a79bee9c0b5
tagnewilfried5 <tagnewilfried5@gmail.com>
2026-06-19 17:59:26 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-apt-get update && apt-get install -y ffmpeg
+apt-get update && apt-get install -y ffmpeg curl wget
+pip install gdown || pip3 install gdown
+gdown 1-gWn3Czkk2Fg6nrjE253BwPdw8Welypw -O video.mp4
 ffmpeg -re -stream_loop -1 -i video.mp4 \
   -c:v libx264 -preset veryfast \
   -b:v 2500k -c:a aac -b:a 128k \
